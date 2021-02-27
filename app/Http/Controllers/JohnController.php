019b9ad005ea867326f6e40907e842003ab6d569
neverza123 <thananat.ma@gmail.com>
2021-02-27 21:14:01 +0700

    commit

--- app/Http/Controllers/JohnController.php
+++ app/Http/Controllers/JohnController.php
@@ -30,4 +30,93 @@ class JohnController extends Controller
             'date' => $stock,
         ], 201);
     }
+    public function EditStock(request $request, $id)
+    {   $name = $request->name;
+        $type = $request->type;
+        $price = $request->price;
+        $qty = $request->qty;
+        $exp = $request->exp;
+
+
+        $stock = Stock::find($id);
+        $stock->name = $name;
+        $stock->type = $type;
+        $stock->price = $price;
+        $stock->qty = $qty;
+        $stock->exp = $exp;
+        $stock->save();
+
+
+        return response()->json([
+            'message' => 'แก้ไขสินค้าใน Stock สำเร็จแล้ว',
+            'date' => $stock,
+        ], 200);
+
+    }
+
+    public function DeleteStock($id)
+    {
+        $stock = Stock::find($id);
+        $stock->delete();
+
+
+        return response()->json([
+            'message' => 'ลบสินค้าใน Stock สำเร็จแล้ว',
+            'data' => '',
+        ], 200);
+
+    }
+
+
+    public function ListStock()
+    {
+        $stock = Stock::all();
+
+        return response()->json([
+            'message' => 'แสดงสินค้าใน Stock สำเร็จแล้ว',
+            'data' => $stock,
+        ], 200);
+
+    }
+
+    public function ShowStock($id)
+    {
+        $stock = Stock::find($id);
+
+
+        return response()->json([
+            'message' => 'แสดงสินค้าใน Stock สำเร็จแล้ว',
+            'data' => $stock,
+        ], 200);
+
+    }
+
+    public function DataTableStock(Request $request)
+    {
+        $columns = $request->input('columns');
+        $length = $request->input('length');
+        $order = $request->input('order');
+        $search = $request->input('search');
+        $start = $request->input('start');
+        $page = $start / $length + 1;
+
+        $col = array('id', 'name', 'type', 'price', 'qty', 'exp');
+
+        $d = Stock::select($col)
+            ->orderby($col[$order[0]['column']], $order[0]['dir']);
+
+        if ($search['value'] != '' && $search['value'] != null) {
+            foreach ($col as &$c) {
+                $d->orWhere($c, 'LIKE', '%' . $search['value'] . '%');
+            }
+        }
+
+        $table = $d->paginate($length, ['*'], 'page', $page);
+
+        return response()->json($table);
+    }
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('add_product', [JohnController::class,'AddProduct'] );
+Route::put('edit_stock/{id}', [JohnController::class,'EditStock'] );
+Route::delete('delete_stock/{id}', [JohnController::class,'DeleteStock'] );
+Route::get('list_stock', [JohnController::class,'ListStock'] );
+Route::get('show_stock/{id}', [JohnController::class, 'ShowStock'] );
+Route::post('datatable_stock', [JohnController::class, 'DataTableStock']);
+
+
 
 //JohnController::class,'AddProduct'] );
